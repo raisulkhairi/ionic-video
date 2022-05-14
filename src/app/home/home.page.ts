1280975901208bdd58b808a1f83311baaa8f423b
unknown <raisulkhairi20@gmail.com>
2022-05-14 08:53:01 +0700

    update home

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import fluidPlayer from 'fluid-player';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -8,5 +8,17 @@ import { Component } from '@angular/core';
 export class HomePage {
 
   constructor() {}
+
+ionViewLoad(){
+ var player = fluidPlayer(
+    'my-video',
+     {
+         layoutControls: {
+             primaryColor: "#28B8ED",
+             posterImage:'https://docs.fluidplayer.com/player/video-thumbnail.jpg'
+         }
+     }n 
+ );
+}
 
 }
